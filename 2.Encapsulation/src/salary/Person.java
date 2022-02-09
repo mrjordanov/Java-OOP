770c1a391212e2f71c1c6cf973d81538f6ccb892
mrjordanov <marin.jordanov@gmail.com>
2022-02-09 15:37:36 +0200

    Last task with unmodifiableLists

--- 2.Encapsulation/src/salary/Person.java
+++ 2.Encapsulation/src/salary/Person.java
@@ -7,10 +7,33 @@ public class Person {
     private double salary;
 
     public Person(String firstName, String lastName, int age, double salary) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setAge(age);
+        this.setSalary(salary);
+    }
+
+    private void setFirstName(String firstName) {
+        ensureNameLength(firstName, "First");
         this.firstName = firstName;
+    }
+
+    private void setLastName(String lastName) {
+        ensureNameLength(lastName, "Second");
         this.lastName = lastName;
+    }
+
+    private void ensureNameLength(String name, String messagePrefix) {
+        if (name.length() < 3) {
+            throw new IllegalArgumentException(messagePrefix + " name cannot be less than 3 symbols");
+        }
+    }
+
+    private void setAge(int age) {
+        if (age <= 0) {
+            throw new IllegalArgumentException("Age cannot be zero or negative integer");
+        }
         this.age = age;
-        this.salary = salary;
     }
 
     public String getLastName() {
@@ -29,7 +52,10 @@ public class Person {
         return salary;
     }
 
-    public void setSalary(double salary) {
+    private void setSalary(double salary) {
+        if (salary < 460) {
+            throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+        }
         this.salary = salary;
     }
 
