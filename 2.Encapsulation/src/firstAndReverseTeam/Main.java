03a171b0f6c1d1f81aee030f744ce1a378c212f4
mrjordanov <marin.jordanov@gmail.com>
2022-02-09 15:38:46 +0200

    Last task with unmodifiableLists

---
+++ 2.Encapsulation/src/firstAndReverseTeam/Main.java
@@ -0,0 +1,34 @@
+package firstAndReverseTeam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+
+        Team team = new Team("Black Eagles");
+
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split(" ");
+
+            try {
+                Person person = new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3]));
+                team.addPlayer(person);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+        System.out.println("First team have " + team.getFirstTeam().size() + " players");
+        System.out.println("Reserve team have " + team.getReserveTeam().size()+ " players");
+
+
+      //  team.getFirstTeam().clear(); ще изплюе грешка защото списъка е unmodifiable!!!!
+
+    }
+}
---
+++ 2.Encapsulation/src/firstAndReverseTeam/Person.java
@@ -0,0 +1,73 @@
+package firstAndReverseTeam;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setAge(age);
+        this.setSalary(salary);
+    }
+
+    private void setFirstName(String firstName) {
+        ensureNameLength(firstName, "First");
+        this.firstName = firstName;
+    }
+
+    private void setLastName(String lastName) {
+        ensureNameLength(lastName, "Second");
+        this.lastName = lastName;
+    }
+
+    private void ensureNameLength(String name, String messagePrefix) {
+        if (name.length() < 3) {
+            throw new IllegalArgumentException(messagePrefix + " name cannot be less than 3 symbols");
+        }
+    }
+
+    private void setAge(int age) {
+        if (age <= 0) {
+            throw new IllegalArgumentException("Age cannot be zero or negative integer");
+        }
+        this.age = age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    private void setSalary(double salary) {
+        if (salary < 460) {
+            throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+        }
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double increment) {
+        if (this.age < 30) {
+            increment = increment / 2;
+        }
+        this.salary += this.salary * increment / 100;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " gets " + salary + " leva";
+    }
+}
