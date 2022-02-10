bfffb43746cb3a65753a8b0c6389218e527a74f5
mrjordanov <marin.jordanov@gmail.com>
2022-02-10 15:34:25 +0200

    Third task

--- 2.Encapsulation/src/shoppingSpree/Main.java
+++ 2.Encapsulation/src/shoppingSpree/Main.java
@@ -1,10 +1,76 @@
 package shoppingSpree;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 public class Main {
     public static void main(String[] args) {
-          Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
+
+        Map<String, Person> personMap = new LinkedHashMap<>();
+
+        String line = scanner.nextLine();
+        String[] inputForCustomers = line.split(";");
+
+        for (String inputForCustomer : inputForCustomers) {
+
+            String[] buyer = inputForCustomer.split("=");
+            String buyerName = buyer[0];
+            double money = Double.parseDouble(buyer[1]);
+            try {
+                Person person = new Person(buyerName, money);
+                personMap.put(buyerName, person);
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+        }
+
+        Map<String, Product> productsMap = new LinkedHashMap<>();
+        String lineForProducts = scanner.nextLine();
+        String[] inputForProducts = lineForProducts.split(";");
+
+        for (String inputForProduct : inputForProducts) {
+
+            String[] products = inputForProduct.split("=");
+            String productName = products[0];
+            double cost = Double.parseDouble(products[1]);
+            try {
+                Product product = new Product(productName, cost);
+                productsMap.put(productName, product);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+        }
+
+        String command = scanner.nextLine();
+        while (!command.equals("END")) {
+
+            String nameOfCustomer = command.split("\\s+")[0];
+            String product = command.split("\\s+")[1];
+
+            try {
+                personMap.get(nameOfCustomer).buyProduct(productsMap.get(product));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            command = scanner.nextLine();
+        }
+
+        for (Map.Entry<String, Person> entry : personMap.entrySet()) {
+
+            System.out.print(entry.getKey() + " - ");
+            String collect = entry.getValue().getProducts().stream().map(Product::getName).collect(Collectors.joining(", "));
+            if (collect.isEmpty()) {
+                System.out.println("Nothing bought");
+            } else {
+                System.out.println(collect);
+            }
+        }
 
     }
 }
---
+++ 2.Encapsulation/src/shoppingSpree/Person.java
@@ -0,0 +1,51 @@
+package shoppingSpree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Person {
+    private String name;
+    private double money;
+    private List<Product> products;
+
+    public Person(String name, double money) {
+        this.setName(name);
+        this.setMoney(money);
+        this.products = new LinkedList<>();
+    }
+
+    private void setName(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
+
+    private void setMoney(double money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void buyProduct(Product product) {
+        if (this.money < product.getCost()) {
+            throw new IllegalArgumentException(this.name + " can`t afford " + product.getName());
+        }
+        this.products.add(product);
+        this.money -= product.getCost();
+        System.out.println(this.name + " bought " + product.getName());
+    }
+
+}
+
+
+
