945b46116265b348426c7e9419d3c31beab84c5e
mrjordanov <marin.jordanov@gmail.com>
2022-02-12 16:44:46 +0200

    last 2 tasks

---
+++ 3.Inheritance/src/animals/Animal.java
@@ -0,0 +1,60 @@
+package animals;
+
+public class Animal {
+    private String name;
+    private int age;
+    private String gender;
+
+    public Animal(String name, int age, String gender) {
+        this.setName(name);
+        this.setAge(age);
+        this.setGender(gender);
+    }
+
+    public void setName(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        if (gender.trim().isEmpty()) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+
+    public String produceSound() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb= new StringBuilder();
+        sb.append(getClass().getSimpleName()).append("\n");
+        sb.append(name).append(" ").append(age).append(" ").append(gender).append("\n");
+        sb.append(produceSound());
+        return sb.toString();
+    }
+}
---
+++ 3.Inheritance/src/restorant/ColdBeverage.java
@@ -0,0 +1,10 @@
+package restorant;
+
+import java.math.BigDecimal;
+
+public class ColdBeverage extends Beverage {
+
+    public ColdBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
