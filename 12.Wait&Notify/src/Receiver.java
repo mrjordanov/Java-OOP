6e07ae8f72fae24daacb7d374b55a7b9b8ee4c2c
mrjordanov <marin.jordanov@gmail.com>
2022-04-06 15:41:10 +0300

    Notify and wait methods example

---
+++ 12.Wait&Notify/src/Receiver.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Receiver implements Runnable {
+    private Data load;
+
+    // standard constructors
+
+
+    public Receiver(Data load) {
+        this.load = load;
+    }
+
+    public void run() {
+        for(String receivedMessage = load.receive();
+            !"End".equals(receivedMessage);
+            receivedMessage = load.receive()) {
+
+            System.out.println(receivedMessage);
+
+            // ...
+            try {
+                Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 5000));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+              //  Log.error("Thread interrupted", e);
+                System.out.println("Thread interrupted");
+            }
+        }
+    }
+}
